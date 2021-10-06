1d0ea1a1b3fba1688c96cc70c7afe91c883c1685
yino <m15829090357@163.com>
2021-10-06 23:23:33 +0800

    db change

--- pkg/schema/message.go
+++ pkg/schema/message.go
@@ -7,10 +7,10 @@ type Message struct {
 	TalkID         ModelPk   `gorm:"type:varchar(100);column:talk_id"        json:"talkId"`
 	MsgFrom        string    `gorm:"type:varchar(16);column:msg_from"        json:"msgFrom"`
 	Origin         uint32    `gorm:"type:tinyint;column:origin"              json:"origin"`
-	Msgid          string    `gorm:"type:varchar(100);column:msgid"          json:"msgid"`
-	Msgtype        string    `gorm:"type:varchar(16);column:msgtype"         json:"msgtype"`
+	Msgid          string    `gorm:"type:varchar(100);column:msg_id"          json:"msgid"`
+	Msgtype        string    `gorm:"type:varchar(16);column:msg_type"         json:"msgtype"`
 	SendTime       time.Time `gorm:"type:timestamp;column:send_time"         json:"sendTime"`
-	ServicerUserid string    `gorm:"type:varchar(100);column:service_userid" json:"serviceUserid"`
+	ServiceUserid string    `gorm:"type:varchar(100);column:service_userid" json:"serviceUserid"`
 	Content        string    `gorm:"type:text;column:content"                json:"content"`
 	Raw            string    `gorm:"type:text;column:raw"                    json:"raw"`
 }
--- sql/init.sql
+++ sql/init.sql
@@ -1,15 +1,33 @@
-CREATE TABLE `tab_wx_kf_log`
+CREATE TABLE `tab_talk`
 (
-    `id`         int(10) unsigned NOT NULL AUTO_INCREMENT,
-    `created_at` timestamp NULL DEFAULT NULL,
-    `updated_at` timestamp NULL DEFAULT NULL,
-    `deleted_at` timestamp NULL DEFAULT NULL,
-    `input`      text         DEFAULT NULL,
-    `response`   varchar(255) DEFAULT NULL,
-    `open_kfid`   varchar(255) DEFAULT NULL,
-    `to_user_id`   int(10) unsigned DEFAULT NULL,
-    `msg_id`      varchar(255) DEFAULT NULL,
-    PRIMARY KEY (`id`),
-    KEY          `idx_users_open_id` (`to_user_id`),
-    KEY          `idx_kf_open_id` (`open_kfid`)
-) ENGINE=InnoDB AUTO_INCREMENT=0 DEFAULT CHARSET=utf8;
+    `id`         int(10)   NOT NULL AUTO_INCREMENT,
+    `created_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    `updated_at` timestamp NULL     DEFAULT CURRENT_TIMESTAMP,
+    `deleted_at` timestamp NULL     DEFAULT CURRENT_TIMESTAMP,
+    `open_kfid`  varchar(100)       DEFAULT NULL,
+    `to_userid`  varchar(100)       DEFAULT NULL,
+    PRIMARY KEY (`id`)
+) ENGINE = InnoDB
+  AUTO_INCREMENT = 0
+  DEFAULT CHARSET = utf8;
+
+
+CREATE TABLE `tab_message`
+(
+    `id`             int(10)   NOT NULL AUTO_INCREMENT,
+    `created_at`     timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    `updated_at`     timestamp NULL     DEFAULT CURRENT_TIMESTAMP,
+    `deleted_at`     timestamp NULL     DEFAULT CURRENT_TIMESTAMP,
+    `talk_id`        INT(10)            DEFAULT NULL,
+    `msg_from`       varchar(100)       DEFAULT NULL,
+    `origin`         tinyint(4)         DEFAULT NULL,
+    `msg_id`         varchar(100)       DEFAULT NULL,
+    `msg_type`       varchar(16)        DEFAULT NULL,
+    `send_time`      timestamp NULL     DEFAULT CURRENT_TIMESTAMP,
+    `service_userid` varchar(100)       DEFAULT NULL,
+    `content`        text               DEFAULT NULL,
+    `raw`            text               DEFAULT NULL,
+    PRIMARY KEY (`id`)
+) ENGINE = InnoDB
+  AUTO_INCREMENT = 0
+  DEFAULT CHARSET = utf8;
