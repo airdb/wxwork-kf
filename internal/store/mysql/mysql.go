b98c91dab0b0c3adf419e423370edcf8ec15d45e
sunxiaohu <sxh01077484@alibaba-inc.com>
2021-10-26 11:33:31 +0800

    删除 mysql get connection

--- internal/store/mysql/mysql.go
+++ internal/store/mysql/mysql.go
@@ -5,7 +5,6 @@ import (
 	"log"
 	"sync"
 
-	"github.com/airdb/sailor/dbutil"
 	"github.com/airdb/wxwork-kf/internal/store"
 
 	"github.com/pkg/errors"
@@ -35,10 +34,7 @@ func (ds *datastore) Close() error {
 
 var (
 	mysqlFactory store.Factory
-	mysqlOnce    sync.Once
 	once         sync.Once
-	readDB       *gorm.DB
-	writeDB      *gorm.DB
 )
 
 // GetFactoryOr create mysql factory with the given config.
@@ -56,14 +52,4 @@ func GetFactoryOr(db *gorm.DB) (store.Factory, error) {
 	}
 
 	return mysqlFactory, nil
-}
-
-// GetConnection  get mysql connection, default is write DB
-func GetConnection() *gorm.DB {
-	mysqlOnce.Do(func() {
-		dbutil.InitDefaultDB()
-	})
-	writeDB = dbutil.WriteDefaultDB()
-	readDB = dbutil.ReadDefaultDB()
-	return writeDB
 }
